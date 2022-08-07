44cff34cd5f87431cf1f3bfa54fabd5b4b1efde9
Shawnbreak <723463195@qq.com>
2022-08-07 22:58:33 +0800

    optimize alpha

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -3,24 +3,25 @@ if not status_ok then
 	return
 end
 
+
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-}
-dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-}
+-- dashboard.section.header.val = {
+-- 	[[                               __                ]],
+-- 	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+-- 	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+-- 	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+-- 	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+-- 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+-- }
+-- dashboard.section.buttons.val = {
+-- 	dashboard.button("SPC f f", "  Find file", ":Telescope find_files <CR>"),
+-- 	dashboard.button("SPC e", "  New file", ":ene <BAR> startinsert <CR>"),
+-- 	dashboard.button("SPC f p", "  Find project", ":Telescope projects <CR>"),
+-- 	dashboard.button("SPC f r", "  Recently used files", ":Telescope oldfiles <CR>"),
+-- 	dashboard.button("SPC f t", "  Find text", ":Telescope live_grep <CR>"),
+-- 	dashboard.button("SPC c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+-- 	dashboard.button("SPC q", "  Quit Neovim", ":qa<CR>"),
+-- }
 
 local function footer()
 -- NOTE: requires the fortune-mod package to work
