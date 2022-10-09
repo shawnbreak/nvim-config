1238b07f8094cd9aae53b392f81f0db2f9f04293
Shawnbreak <723463195@qq.com>
2022-10-09 22:28:59 +0800

    change mason template url

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -8,7 +8,8 @@ mason.setup({
         -- 1. The repository (e.g. "rust-lang/rust-analyzer")
         -- 2. The release version (e.g. "v0.3.0")
         -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
-        download_url_template = "https://hub.fastgit.xyz/%s/releases/download/%s/%s",
+        -- download_url_template = "https://hub.fastgit.xyz/%s/releases/download/%s/%s",
+        download_url_template = "https://hub.gitfast.tk/%s/releases/download/%s/%s",
     },
 })
 
